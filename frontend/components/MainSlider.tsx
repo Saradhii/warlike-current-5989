7d89d9a56cb3191accab38e39f92e544a9c0b211
Muskan Shaw <princesahil0065@gmail.com>
2022-07-19 23:05:25 +0530

    Main Slider done

---
+++ frontend/components/MainSlider.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "../styles/Home.module.css"
+
+const MainSlider = () => {
+  return (
+    <div className={style.MainSlider}>
+        
+    </div>
+  )
+}
+
+export default MainSlider
