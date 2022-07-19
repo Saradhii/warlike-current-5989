c0ad6a74698d97a0d1c2b20b94a466a5accc1aea
Devesh Goyal <Devesh26goyal@gmail.com>
2022-07-19 13:29:07 +0530

    added base structure

---
+++ backend/index.ts
@@ -0,0 +1,25 @@
+import http from "http";
+import express, { Application, Request, Response } from "express";
+import bodyParser from "body-parser";
+import connection from "./server/db";
+import cors from "cors";
+
+const PORT = process.env.PORT || 8080;
+
+const app: Application = express();
+const server = http.createServer(app);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome");
+});
+
+server.listen(PORT, () => {
+  connection.then((con) => {
+    console.log(`Server is running on PORT http://localhost:${PORT}`);
+  });
+});
