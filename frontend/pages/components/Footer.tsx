7591a443a1de884ae9bbd7e4032d996e30387f46
Bhushan Wagh <wagh.bhushan.998@gmail.com>
2022-07-20 21:05:03 +0530

    approx done footer

---
+++ frontend/pages/components/Footer.tsx
@@ -0,0 +1,53 @@
+import { NextPage } from "next";
+import styles from "./footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { fa0 } from "@fortawesome/free-solid-svg-icons";
+const Footer: NextPage = () => {
+  return (
+    <div className={styles.footermaincont}>
+      <div className={styles.footerContainer}>
+        <div>
+          <ul>
+            <h3 className={styles.footerH3}>INFORMATION</h3>
+            <li>About Us</li>
+            <li>Associated Brands</li>
+            <li>FAQ</li>
+            <li>Collaborate with Fab Bag</li>
+            <li>Contact Us</li>
+            <li>Returns & Refunds</li>
+            <li>Terms & Conditions</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className={styles.footerH3}>CUSTOMER CARE</h3>
+          <h5>
+            Call : <b className={styles.enum}>022-6805-6010</b>
+            <br />
+            (Monday to Friday : 9 AM - 7 PM).
+          </h5>
+          <h5>
+            Mail : <b className={styles.enum}>crew@fabbag.com</b>
+          </h5>
+        </div>
+        <div>
+          <h3 className={styles.footerH3}>NEWSLETTER</h3>
+          <p>Signup for the latest offers, updates and news</p>
+          <input type="text" placeholder="Your email address" />
+          <button>subscribe</button>
+          <p>Don’t worry we don’t spam</p>
+        </div>
+      </div>
+      <div className={styles.socialmedia}>
+        <div>
+          <FontAwesomeIcon icon={fa0} />
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+      <p>Copyright © 2021 fabbag.com All rights reserved.</p>
+    </div>
+  );
+};
+export default Footer;
