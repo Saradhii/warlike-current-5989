48d79174a1d2fc58bbb8806e20bd9454d976f822
gaganrajput7 <gaganrajputbapora@gmail.com>
2022-07-24 09:33:43 +0530

    commit

--- frontend/pages/myaccount/index.tsx
+++ frontend/pages/myaccount/index.tsx
@@ -1,34 +1,33 @@
-import React from 'react'
-import styles from '../../styles/header.module.css'
-import Link from 'next/link'
+import React from "react";
+import styles from "../../styles/header.module.css";
+import Link from "next/link";
 export default function Account() {
   return (
     <>
-    
-<div className={styles.mainAccount}>
-<p>My Account</p>
-<hr />
-<span>MY BEAUTY PROFILE</span><Link href='/'>Click Here</Link>
-<hr />
-<span>MY PRODUCT REVIEW</span><Link href='/'>Click Here</Link>
-<hr />
-<hr />
-<p>ORDER HISTORY</p>
-<br />
-<ul>You haven't placed any orders yet.</ul>
-<div>
-  <p>ACCOUNT DETAILS</p>
-  <hr />
-<p>gagan</p>
+      <div className={styles.mainAccount}>
+        <p>My Account</p>
+        <hr />
+        <span>MY BEAUTY PROFILE</span>
+        <Link href="/">Click Here</Link>
+        <hr />
+        <span>MY PRODUCT REVIEW</span>
+        <Link href="/">Click Here</Link>
+        <hr />
+        <hr />
+        <p>ORDER HISTORY</p>
+        <br />
+        <ul>You haven't placed any orders yet.</ul>
+        <div>
+          <p>ACCOUNT DETAILS</p>
+          <hr />
+          <p>gagan</p>
+        </div>
 
-</div>
-
-<div>
-  <p>India</p>
-  <button>VIEW ADDRESSES</button>
-</div>
-</div>
-
+        <div>
+          <p>India</p>
+          <button>VIEW ADDRESSES</button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
